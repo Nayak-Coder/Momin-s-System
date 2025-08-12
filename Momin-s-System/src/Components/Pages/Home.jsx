72c1ebed9ff31a7499404a0096eaea8de2cd8b27
Muhammad Estehsan Ul Haq <estehsanul@gmail.com>
2025-08-12 10:43:33 +0500

    Update Home.jsx

--- Momin-s-System/src/Components/Pages/Home.jsx
+++ Momin-s-System/src/Components/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Welcome to the Home Page!!</h1>
       {/* Add more content for your home page here */}
     </div>
   );
