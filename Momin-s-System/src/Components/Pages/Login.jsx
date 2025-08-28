12b54409459e6b65893ac598b93f8dca8b11e3d5
Estehan198 <estehsanul@gmail.com>
2025-08-28 18:54:00 +0500

    Update login

--- Momin-s-System/src/Components/Pages/Login.jsx
+++ Momin-s-System/src/Components/Pages/Login.jsx
@@ -10,7 +10,7 @@ const Loginpage = () => {
                         <label className="
                         block text-sm font-medium text-gray-700/>">Email</label>
                         <input
-                        type ="email"
+                        type ="string"
                         placeholder="Email"
                         classname ="w-full px-3 py-2 border border gray-300 rounded mb-3 focus;outline none focus:ring-2 focus:ring-blue-500"
                         />
@@ -19,12 +19,23 @@ const Loginpage = () => {
                         <label className="
                         block text-sm font-medium text-gray-700/>">Password</label>
                         <input
-                        type ="password"
+                        type ="string"
                         placeholder="Password"
                         classname ="w-full px-3 py-2 border border gray-300 rounded mb-3 focus;outline none focus:ring-2 focus:ring-blue-500"
                         />
                     </div>
                 </form>
+                 
+                   <button
+                     type="submit"  
+                        className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
+                     onClick={(e)=> {e.preventDefault();
+                     console.log("Login button clicked");
+                     console.log("Form submitted");
+                        window.location.href='/'
+                     } }>
+                        Login
+                     </button>  
             </div>
         </div>
     )
