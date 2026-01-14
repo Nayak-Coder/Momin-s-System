fe1fd5bd218369d5bb73cd274536f818378edb81
Estehan198 <estehsanul@gmail.com>
2026-01-14 10:35:25 +0500

    same context of feature

---
+++ Momin-s-System/src/Context/WishlistContext.jsx
@@ -0,0 +1,35 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const WishlistContext = createContext();
+
+export const WishlistProvider = ({ children }) => {
+  const [wishlist, setWishlist] = useState([]);
+
+  const addToWishlist = (product) => {
+    if (!wishlist.find(item => item.id === product.id)) {
+      setWishlist([...wishlist, product]);
+    }
+  };
+
+  const removeFromWishlist = (productId) => {
+    setWishlist(wishlist.filter(item => item.id !== productId));
+  };
+
+  const isInWishlist = (productId) => {
+    return wishlist.some(item => item.id === productId);
+  };
+
+  return (
+    <WishlistContext.Provider value={{ wishlist, addToWishlist, removeFromWishlist, isInWishlist }}>
+      {children}
+    </WishlistContext.Provider>
+  );
+};
+
+export const useWishlist = () => {
+  const context = useContext(WishlistContext);
+  if (!context) {
+    throw new Error('useWishlist must be used within WishlistProvider');
+  }
+  return context;
+};
